076fd2b23c9d2049959d79a22a63438121fd3148
varun199614 <58078466+varun199614@users.noreply.github.com>
2020-01-10 22:54:40 +0530

    ribbon implements
    dynamic scaleing

--- src/main/java/com/project2/microservices/currencycalculationservice/rescorceDAO/feignProxyInterface.java
+++ src/main/java/com/project2/microservices/currencycalculationservice/rescorceDAO/feignProxyInterface.java
@@ -1,12 +1,15 @@
 package com.project2.microservices.currencycalculationservice.rescorceDAO;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.project2.microservices.currencycalculationservice.beans.CalculatingBean;
 
-@FeignClient(name="current-convertionrate-service",url="http://localhost:8000")
+//@FeignClient(name="current-convertionrate-service",url="http://localhost:8000")
+@FeignClient(name="current-convertionrate-service")
+@RibbonClient(name="current-convertionrate-service")
 public interface feignProxyInterface {
 	@GetMapping("exchangeValue/{exFrom}/to/{exTo}")
 	public CalculatingBean ExchangeValue(@PathVariable String exFrom,@PathVariable String exTo);
