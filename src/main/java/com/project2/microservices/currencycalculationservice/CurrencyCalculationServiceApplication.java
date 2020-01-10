5b2a70b4108343a7d36aaf71876c9d4caddee7b3
varun199614 <58078466+varun199614@users.noreply.github.com>
2020-01-11 02:08:52 +0530

    using eureka naming server
    final product with naming server ,feign and ribbon work to gether.

--- src/main/java/com/project2/microservices/currencycalculationservice/CurrencyCalculationServiceApplication.java
+++ src/main/java/com/project2/microservices/currencycalculationservice/CurrencyCalculationServiceApplication.java
@@ -2,11 +2,13 @@ package com.project2.microservices.currencycalculationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableFeignClients("com.project2.microservices.currencycalculationservice.rescorceDAO")
+@EnableDiscoveryClient
 public class CurrencyCalculationServiceApplication {
 
 	public static void main(String[] args) {
